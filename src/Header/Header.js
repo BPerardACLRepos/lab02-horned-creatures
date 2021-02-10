dedb11598b09ade81e99554eb9e2a61aa8fb3c24
Brandon Perard <brandon.perard@gmail>
2021-02-09 18:44:55 -0800

    update Header component content and styling

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,8 +4,13 @@ import './Header.css';
 export default class Header extends React.Component {
     render() {
         return (
-            <div>
-
+            <div className='header-container'>
+                <h1>
+                    Horned Creatures
+                </h1>
+                <p>
+                    ...and how to filter them
+                </p>
             </div>
         );
     }
