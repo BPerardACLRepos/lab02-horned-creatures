79c6103f216f3f38f9fb20d205153b43ea52c200
Brandon Perard <brandon.perard@gmail>
2021-02-09 20:52:15 -0800

    add map function to render this.props.imagesArray

--- src/ImageList/ImageList.js
+++ src/ImageList/ImageList.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import './ImageList.js';
+import './ImageList.css';
 
 export default class ImageList extends React.Component {
     render() {
+
+        const filteredImages = this.props.imagesArray;
+
+        const imageElements = filteredImages.map(animal =>
+            <label key={animal.title} >
+                <h3>{animal.title}</h3>
+                <img src={animal.url} alt={animal.title} />
+                <p>{animal.description}</p>
+            </label>
+        );
+
         return (
-            <div>
-                {this.props.imagesArray.length}
+            <div className='hornedCreatureDiv'>
+                {imageElements}
             </div>
         );
     }
